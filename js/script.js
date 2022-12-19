0230e1a437c6133873552fdcdd749d19988cd7a9
Salvatore Capano <capano_salvatore@libero.it>
2022-12-19 14:07:07 +0100

    -Agg. inserimento km -Agg. inserimento eta'

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Inserire n. km da percorrere
+const distance = parseInt(prompt("Quanti km vuoi percorrere?"));
+
+// Inserire eta' passeggero
+const age = parseInt(prompt("Quanti anni hai?"));
+
+/* 
+   Calcolo prezzo del viaggio:
+    a) 0.21€ al km
+    b) -20% se eta' < 18
+    c) -40% se eta' > 65
+*/
+
+// Eliminazione cifre decimali oltre la seconda
+
+// Mostrare prezzo finale
